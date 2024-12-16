4982227820b4c8b40b5675db8822e0b9dcac78ae
4ndresdev <4ndresdev@gmail.com>
2024-12-15 18:38:02 -0600

    Refactor WallpaperStep component to include wallpaper selection and enhance layout; update import path for wallpaper image in Onboarding page

--- src/components/steps/WallpaperStep.jsx
+++ src/components/steps/WallpaperStep.jsx
@@ -1,37 +1,78 @@
 import { ArrowRight } from "lucide-react";
-import useNameStep from "@/hooks/useNameStep";
 import ButtonWithIcon from "@/components/buttons/ButtonWithIcon";
+import { Image } from "@nextui-org/react";
+import wallpaper from "@/assets/images/wallpapers/wallpaper.webp";
+import wallpaper3 from "@/assets/images/wallpapers/wallpaper3.webp";
 
 const NameStep = () => {
-  const { nameRef, handleInput, handleKeyDown, isDisabled } = useNameStep();
   return (
-    <div className="w-full h-full p-8 flex flex-col items-center gap-2 bg-white rounded-xl shadow-xl fade-in delay-200ms">
-      <div className="w-14 h-14 bg-white flex justify-center items-center rounded-md text-3xl shadow-inner-custom fade-in delay-500ms">
-        🪴
+    <div className="w-full h-full p-5 flex flex-col items-center gap-2 bg-white rounded-xl shadow-xl fade-in delay-200ms">
+      <div className="w-full flex gap-2">
+        <div className="w-14 h-14 bg-white flex justify-center items-center rounded-md text-xl shadow-inner-custom fade-in delay-500ms">
+          🎨
+        </div>
+        <div className="flex flex-col justify-center mr-2">
+          <h1 className="text-lg">We are almost done 👏</h1>
+          <small className="text-sm text-gray-500 fade-in delay-1000ms">
+            Please select a wallpaper
+          </small>
+        </div>
       </div>
-      <h1 className="text-2xl font-medium text-center fade-in delay-1000ms">
-        Welcome to Planty!
-      </h1>
-      <p className="text-center text-sm text-gray-500 fade-in delay-1000ms">
-        Please enter your name ✏️
-      </p>
-      <h1
-        ref={nameRef}
-        className="w-56 text-2xl font-bold text-center focus:outline-none focus:ring-0 bg-transparent placeholder_name fade-in delay-1500ms"
-        role="textbox"
-        aria-label="Name input field"
-        contentEditable
-        onInput={handleInput}
-        onKeyDown={handleKeyDown}
-        suppressContentEditableWarning={true}
-      ></h1>
-      <div className="mt-5 fade-in delay-1500ms">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-2 mt-2">
+        <div className="fade-in delay-500ms">
+          <Image
+            className="h-24 max-w-full cursor-pointer border-3 rounded-2xl border-warning"
+            src={wallpaper}
+            alt=""
+            isZoomed
+            isBlurred
+          />
+        </div>
+        <div className="fade-in delay-1000ms">
+          <Image
+            className="h-24 max-w-full rounded-lg cursor-pointer"
+            src={wallpaper3}
+            alt=""
+            isZoomed
+            isBlurred
+          />
+        </div>
+        <div className="fade-in delay-1500ms">
+          <Image
+            className="h-24 max-w-full rounded-lg cursor-pointer"
+            src={wallpaper}
+            alt=""
+            isZoomed
+            isBlurred
+          />
+        </div>
+        <div className="fade-in delay-2000ms">
+          <Image
+            className="h-24 max-w-full rounded-lg cursor-pointer"
+            src={wallpaper}
+            alt=""
+            isZoomed
+            isBlurred
+          />
+        </div>
+      </div>
+      <div className="w-full flex justify-end mt-5 fade-in delay-1500ms gap-2">
+        <div className="rotate-180">
+          <ButtonWithIcon
+            variant="flat"
+            color="warning"
+            size="lg"
+            ariaLabel="Back step"
+          >
+            <ArrowRight strokeWidth={3} />
+          </ButtonWithIcon>
+        </div>
         <ButtonWithIcon
-          isDisabled={isDisabled}
           variant="shadow"
           color="primary"
           size="lg"
           ariaLabel="Next step"
+          isDisabled
         >
           <ArrowRight strokeWidth={3} />
         </ButtonWithIcon>
